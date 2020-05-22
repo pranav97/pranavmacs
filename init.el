fcbd924b0e5a7a84a9717d836f8d7fea0fe23956
Pranav Raghavan <gtalkpranav@gmail.com>
2020-05-22 10:53:38 -0700

    increased delay and separate require file

--- init.el
+++ init.el
@@ -7,20 +7,30 @@
 (add-to-list 'load-path "~/emacs_config/external")
 (package-initialize)
 
-(require 'which-key)
-(require 'nlinum-relative)
-
+(load-file "~/emacs_config/requirements.el")
 
 (load-file "~/emacs_config/my_evil.el")
 
 (nlinum-relative-setup-evil)                    ;; setup for evil
 (add-hook 'prog-mode-hook 'nlinum-relative-mode)
-(setq nlinum-relative-redisplay-delay 0)      ;; delay
+(setq nlinum-relative-redisplay-delay 0.2)      ;; delay
 (setq nlinum-relative-current-symbol "0")      ;; or "" for display current line number
 (setq nlinum-relative-offset 0)                 ;; 1 if you want 0, 2, 3...
 
 
 (auto-save-visited-mode)
-(which-key-mode 1)
-
 (which-key-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (evil-magit which-key org-evil nlinum-relative evil-nerd-commenter))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ requirements.el
@@ -0,0 +1,4 @@
+(require 'which-key)
+(require 'nlinum-relative)
+(require 'org-evil)
+(require 'evil-magit)
