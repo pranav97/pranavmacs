6d7f6487e700097eec8fa4813e0239aa6e026be2
Pranav Raghavan <gtalkpranav@gmail.com>
2020-06-10 05:54:46 -0700

    refactor remove useless

--- init.el
+++ init.el
@@ -46,7 +46,25 @@
 (load-file "~/.emacs.d/key_bindings.el")
 
 ;; helm configurations
-(load-file "~/.emacs.d/my_helm.el")
+;; helm stuff
+(use-package helm-config
+  :config
+    (helm-mode 1)
+    ;; projectile locate in OS x
+    (setq helm-locate-command
+	"glocate %s %s"
+	helm-locate-create-db-command
+	"gupdatedb --output='%s' --localpaths='%s'")
+
+    (setq helm-locate-project-list
+	(list "/Users/pranav_raghavan/emacs_config/"))
+
+    ;; required to track visited projects and put them in helm-projectile-switch
+    (projectile-global-mode)
+    (require 'helm-projectile)
+    (setq projectile-completion-system 'helm)
+    (helm-projectile-on))
+
 
 (use-package org-evil :ensure t)
 (use-package magit :ensure t)
@@ -134,6 +152,9 @@
     (ac-config-default)
     ;; auto complete turns on suggestions when you are writing code
     (global-auto-composition-mode))
+
+
+
 
 
 
